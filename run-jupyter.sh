841080dfc6bcdc5b106899f80d30342f8226cfb4
Kenneth Hill <ken.hill@nimbix.net>
2019-03-20 16:51:50 -0500

    Use smaller token size
    Signed-off-by: Kenneth Hill <ken.hill@nimbix.net>

--- run-jupyter.sh
+++ run-jupyter.sh
@@ -3,4 +3,5 @@ export PATH=/usr/local/lib/anaconda2/bin:$PATH
 conda activate jupyter-env 
 pip install --user jupyter
 # Start jupyter notebook
-jupyter notebook --port=8888 --ip=0.0.0.0 --no-browser --certfile=/etc/JARVICE/cert.pem --NotebookApp.token=`cat /etc/JARVICE/random128.txt`
+
+jupyter $notebook --port=8888 --ip=0.0.0.0 --no-browser --certfile=/etc/JARVICE/cert.pem --NotebookApp.token=`cat /etc/JARVICE/random128.txt | cut -c 1-16`
